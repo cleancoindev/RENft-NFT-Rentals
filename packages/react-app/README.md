d34ea3e6daa46a7ce96e01c81bf27524b8176278
nazariyv <nazariy@inbox.ru>
2020-09-18 17:39:42 +0100

    react + typescript + storybook = pure functional components that any dApp developer can just plug in their app. oh my, few understand

---
+++ packages/react-app/README.md
@@ -0,0 +1,15 @@
+# React + Typescript@4 + Storybook
+
+The idea of the components here is to have atomic pure components that any dApp project can just grab and build from. We want this to be used as a React template library
+
+## Install
+
+```bash
+yarn
+```
+
+## Storybook
+
+```bash
+yarn storybook
+```
---
+++ packages/react-app/src/stories/Header.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+// import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from '@storybook/react';
+
+import { Header, HeaderProps } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+} as Meta;
+
+const Template: Story<HeaderProps> = () => <Header />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
