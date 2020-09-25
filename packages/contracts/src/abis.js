cc25e935da0ed320ca3ba0df64f2eb2fe8308ca5
nazariyv <nazariy@inbox.ru>
2020-09-25 10:28:58 +0100

    smart contract abi and address

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,7 @@
-import erc20Abi from "./abis/erc20.json";
-import ownableAbi from "./abis/ownable.json";
+import renft from "./abis/renft.json";
 
 const abis = {
-  erc20: erc20Abi,
-  ownable: ownableAbi,
+  renft: renftAbi,
 };
 
 export default abis;
--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,7 +1,7 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  renft: "0xb2c98f7f573bbf653972f030766e36138c82f4a2",
 };
 
 export default addresses;
