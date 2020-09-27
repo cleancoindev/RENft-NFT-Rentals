7a5194ec37e5e20fb86a7b3512606d78fff9a1dd
nazariyv <nazariy@inbox.ru>
2020-09-27 17:43:44 +0100

    dynamic overview

--- packages/react-app/src/pages/overview/components/AvaAndAdress.tsx
+++ packages/react-app/src/pages/overview/components/AvaAndAdress.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import { Typography } from '@material-ui/core';
 import { deepOrange, green } from '@material-ui/core/colors';
 import Pretty1 from '../../../assets/img/pretty1.svg';
 
@@ -29,6 +30,7 @@ const AvaAndAdress: React.FC = () => {
       <Avatar variant="rounded" className={classes.rounded}>
         <img alt="lady hear me tonight" src={Pretty1} />
       </Avatar>
+      <Typography>0x1237821381278...</Typography>
     </div>
   );
 };
